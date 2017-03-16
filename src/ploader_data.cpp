6ec682c6921aef3fbbfe58cfbfa1b8bf7ab07b5d
David (Pololu) <dev-david@pololu.com>
2017-03-16 14:20:43 -0700

    Added Tic support.

--- src/ploader_data.cpp
+++ src/ploader_data.cpp
@@ -6,9 +6,11 @@ enum PloaderTypeIds
     ID_P_STAR_25K50_BOOTLOADER,
     ID_PGM04A_BOOTLOADER,
     ID_PGM04A_APP,
+    ID_TIC_BOOTLOADER,
+    ID_TIC_APP,
 #ifndef NDEBUG
-    ID_SMC_18V25_APP,
     ID_SMC_18V25_BOOTLOADER,
+    ID_SMC_18V25_APP,
 #endif
 };
 
@@ -19,6 +21,14 @@ const std::vector<PloaderAppType> ploaderAppTypes = {
         /* usbProductId */ 0x00B0,
         /* name */ "Pololu USB AVR Programmer v2",
         /* composite */ true,
+        /* interfaceNumber */ 0,
+    },
+    {
+        /* id */ ID_TIC_APP,
+        /* usbVendorId */ 0x1FFB,
+        /* usbProductId */ 0x00B3,
+        /* name */ "Pololu Tic", // TODO
+        /* composite */ false,
         /* interfaceNumber */ 0,
     },
 #ifndef NDEBUG
@@ -70,6 +80,24 @@ const std::vector<PloaderType> ploaderTypes = {
         /* deviceCode */ NULL,
         /* matchingAppTypes */ { ID_PGM04A_APP },
     },
+    {
+        /* id */ ID_TIC_BOOTLOADER,
+        /* usbVendorId */ 0x1FFB,
+        /* usbProductId */ 0x00B2,
+        /* name */ "Pololu Tic Bootloader", // TODO
+        /* appAddress */ 0x2000,
+        /* appSize */ 0x6000,
+        /* writeBlockSize */ 0x40,
+        /* erasingFlashAffectsEeprom */ true,
+        /* supportsFlashPlainWriting */ false,
+        /* supportsFlashReading */ false,
+        /* eepromAddress */ 0,
+        /* eepromAddressHexFile */ 0xF00000,
+        /* eepromSize */ 0x100,
+        /* supportsEepromAccess */ true,
+        /* deviceCode */ NULL,
+        /* matchingAppTypes */ { ID_TIC_APP },
+    },
 #ifndef NDEBUG
     {
         /* id */ ID_SMC_18V25_BOOTLOADER,
@@ -107,6 +135,11 @@ const std::vector<PloaderUserType> ploaderUserTypes = {
         /* codeName */ "pgm04a",
         /* name */ "Pololu USB AVR Programmer v2",
         /* memberIds */ { ID_PGM04A_BOOTLOADER, ID_PGM04A_APP },
+    },
+    {
+        /* codeName */ "tic",
+        /* name */ "Pololu Tic", // TODO
+        /* memberIds */ { ID_TIC_BOOTLOADER, ID_TIC_APP },
     },
 #ifndef NDEBUG
     {
